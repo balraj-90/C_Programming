295f84d03216f5145b06043712f796d2aff6abe3
balraj-90 <balrajjagtap9900@gmail.com>
2025-11-29 12:37:05 +0530

    add solutions of assignment 30

---
+++ Assignments/Assignment_30/Assignment30_1.c
@@ -0,0 +1,62 @@
+/*
+    Input : iRow = 4 iCol = 4
+
+    Output :        *  #   #   #
+                    *  *   #   #
+                    *  *   *   #
+                    *  *   *   *     
+*/
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//      Function Name : Pattern
+//      Description :   Pattern Printing 
+//      Input :         Integer, Integer 
+//      Output :        Pattern Printed
+//      Author :        Balraj Umakant Jagtap
+//      Date :          24/11/2025
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)                   //Function Definition
+{
+    int i = 0, j= 0;                               //Variable Declaration       
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if( i >= j )
+            {
+                printf("*\t");
+            }
+            else 
+            {
+                printf("#\t");
+            }
+        }
+        printf("\n");
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//      Entry Point function for the Application
+//
+//////////////////////////////////////////////////////////////////////////////
+
+int main()                                      //Main Function
+{
+    int iValue1 = 0;                            //Variable Declaration
+    int iValue2 = 0;
+
+    printf("Enter the Number of Rows and Columns : \n");
+    scanf("%d %d",&iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);                  //Method Call
+
+    return 0; 
+}
---
+++ Assignments/Assignment_30/Assignment30_5.c
@@ -0,0 +1,62 @@
+/*
+    Input : iRow = 4 iCol = 4
+
+    Output :    1   2   3   4
+                1   *   *   4
+                1   *   *   4
+                1   2   3   4
+*/
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//      Function Name : Pattern
+//      Description :   Pattern Printing 
+//      Input :         Integer, Integer 
+//      Output :        Pattern Printed
+//      Author :        Balraj Umakant Jagtap
+//      Date :          24/11/2025
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)                   //Function Definition
+{
+    int i = 0, j= 0;                               //Variable Declaration       
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if( i == 1 || i == iRow || j == 1 || j == iCol  )
+            {
+                printf("%d\t", j);
+            }
+            else 
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//      Entry Point function for the Application
+//
+//////////////////////////////////////////////////////////////////////////////
+
+int main()                                      //Main Function
+{
+    int iValue1 = 0;                            //Variable Declaration
+    int iValue2 = 0;
+
+    printf("Enter the Number of Rows and Columns : \n");
+    scanf("%d %d",&iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);                  //Method Call
+
+    return 0; 
+}
